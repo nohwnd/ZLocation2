6869c84932395465941a18243f7f4f6930adaa87
Jakub Jareš <me@jakubjares.com>
2025-04-17 12:11:24 +0200

    workaround pester bug with relative path

--- test.ps1
+++ test.ps1
@@ -15,4 +15,13 @@ Remove-Item $PSScriptRoot/testdb.db -ErrorAction Ignore
 Get-Module ZLocation2 | Remove-Module
 
 Import-Module $PSScriptRoot/ZLocation2/ZLocation2.psd1
-Invoke-Pester -Output Detailed -CI:$CI
+
+$configuration = New-PesterConfiguration
+$configuration.Output.Verbosity = 'Detailed'
+
+if ($CI) {
+    $configuration.Run.Exit = $true
+    $configuration.TestResult.Enabled = $true
+}
+
+Invoke-Pester -Configuration $configuration
